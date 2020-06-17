ac68a8bc69edbdd442749b8c498ca0697347c39d
Sebastian Machaj <team@blackredstudio.com>
2020-06-17 20:49:23 +0200

    added sigin form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
-import './styles/App.scss';
-import HomePage from './pages/HomePage';
-import ShopPage from './pages/ShopPage';
-import Header from './components/Header'
+import "./styles/App.scss";
+import HomePage from "./pages/HomePage";
+import ShopPage from "./pages/ShopPage";
+import Header from "./components/Header";
+import LoginRegisterPage from "./pages/LoginRegisterPage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={LoginRegisterPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/LoginRegisterPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignIn from '../components/SignIn'
+
+const LoginRegisterPage = () => {
+    return (
+        <div className="login-register">
+            <SignIn />
+        </div>
+    )
+}
+
+export default LoginRegisterPage
