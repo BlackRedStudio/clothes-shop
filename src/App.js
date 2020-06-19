a95273eddd509e5677a5d0f28a7fb4038f98e2b2
Sebastian Machaj <team@blackredstudio.com>
2020-06-19 14:54:05 +0200

    implemented firebase sign up

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from "./pages/HomePage";
 import ShopPage from "./pages/ShopPage";
 import Header from "./components/Header";
 import LoginRegisterPage from "./pages/LoginRegisterPage";
-import { auth } from './firebase/firebase-utils'
+import { auth, createUserProfileDocument } from './firebase/firebase-utils'
 
 class App extends Component {
 
@@ -17,8 +17,22 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user})
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        });
+      } else {
+        this.setState({ currentUser: null})
+      }
+        
     })
   }
 
--- src/pages/LoginRegisterPage.js
+++ src/pages/LoginRegisterPage.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import SignIn from '../components/SignIn'
+import SignUp from '../components/SignUp'
+import '../styles/LoginRegisterPage.scss'
 
 const LoginRegisterPage = () => {
     return (
         <div className="login-register">
             <SignIn />
+            <SignUp />
         </div>
     )
 }
