c6fa873a8bacec5f5474cd5b14bead81584ef96e
Sebastian Machaj <team@blackredstudio.com>
2020-06-26 12:51:57 +0200

    added addItem and removeItem functionality to checkout item component

--- src/redux/reducers/cart-reducer.js
+++ src/redux/reducers/cart-reducer.js
@@ -1,27 +1,38 @@
-import { cartActionTypes } from '../types/cart-types'
-import { addItemToCart } from '../utils/cart-utils'
+import { cartActionTypes } from "../types/cart-types";
+import { addItemToCart, removeItemFromCart } from "../utils/cart-utils";
 
 const INITIAL_STATE = {
-    hidden: true,
-    cartItems: []
-}
+	hidden: true,
+	cartItems: [],
+};
 
 const cartReducer = (state = INITIAL_STATE, action) => {
-    
-    switch (action.type) {
-        case cartActionTypes.TOGGLE_CART_HIDDEN:
-            return {
-                ...state,
-                hidden: !state.hidden
-            }
-        case cartActionTypes.ADD_ITEM:
-            return {
-                ...state,
-                cartItems: addItemToCart(state.cartItems, action.payload)
-            }
-        default:
-            return state;
-    }
-}
+	switch (action.type) {
+		case cartActionTypes.TOGGLE_CART_HIDDEN:
+			return {
+				...state,
+				hidden: !state.hidden,
+			};
+		case cartActionTypes.ADD_ITEM:
+			return {
+				...state,
+				cartItems: addItemToCart(state.cartItems, action.payload),
+			};
+		case cartActionTypes.REMOVE_ITEM:
+			return {
+				...state,
+				cartItems: removeItemFromCart(state.cartItems, action.payload),
+			};
+		case cartActionTypes.CLEAR_ITEM_FROM_CART:
+			return {
+				...state,
+				cartItems: state.cartItems.filter(
+					cartItem => cartItem.id !== action.payload.id
+				),
+			};
+		default:
+			return state;
+	}
+};
 
 export default cartReducer;
