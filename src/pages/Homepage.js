7a71caac03e36af95abd5cb2c801a32dca741e67
Sebastian Machaj <team@blackredstudio.com>
2020-07-03 14:43:04 +0200

    test

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import '../styles/HomePage.scss'
-import '../components/Directory'
-import Directory from '../components/Directory'
+import React from 'react';
+import '../styles/HomePage.scss';
+import '../components/Directory';
+import Directory from '../components/Directory';
 
 const HomePage = () => {
-    return (
-        <div className="homepage">
-            <div className="directory-menu">
-                <Directory />
-            </div>
-        </div>
-    )
-}
+	return (
+		<div className="homepage">
+			<div className="directory-menu">
+				<Directory />
+			</div>
+		</div>
+	);
+};
 
-export default HomePage
+export default HomePage;
